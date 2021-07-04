691ae6f9a2e619dca45778db922f657e027c51c5
Adbistju <adjytant@gmail.com>
2021-07-04 22:33:26 +0300

    .

--- src/main/java/adbistju/system/mysql/MainController.java
+++ src/main/java/adbistju/system/mysql/MainController.java
@@ -5,11 +5,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
+
 @Controller
 @RequestMapping(path="/")
 public class MainController {
-    @Autowired
+
     private UserRepository userRepository;
+
+    @Autowired
+    public MainController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @PostMapping(path="/add")
     public @ResponseBody String addNewUser (@RequestParam String name
